4e3163fbcbc1560072d0d357e1f57d07b35ac127
Saur-av <150913593+Saur-av@users.noreply.github.com>
2023-12-29 20:13:15 +0545

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,6 @@ import re
 from bs4 import BeautifulSoup
 from seleniumbase import SB
 
-def get_discount(source : str) -> list:
-    print("Getting dicount...")
-    #the website comments the code, so we have to use regex to find the links from the source
-    pattern = r'" ?(/offer[^"]*)"'
-    matches = re.findall(pattern, source)
-    matches.pop() #Last element is always a unwanted link
-    return [match for match in matches]
-        
 def enroll_udemy(courseurls : list) -> list:
     with SB(uc=True) as sb:
         sb.driver.get("https://www.udemy.com/")
@@ -32,30 +24,6 @@ def enroll_udemy(courseurls : list) -> list:
                 print("Course is not free!")
             sb.sleep(1)
         return enrolled
-    
-def get_source() -> str:
-    with SB(uc=True) as sb:
-        print("Getting source...")
-        sb.driver.get("https://www.real.discount/")
-        sb.driver.click("label.tgl-btn") #free button
-        try:
-            sb.driver.click('input[value="Load More"]') #gets more link
-            sb.driver.click('input[value="Load More"]')
-        except Exception as e:
-            print(e)
-        finally:
-            return sb.driver.page_source
-
-def get_links(url):
-    for link in url:
-        response = requests.get('https://real.discount'+link)
-        soup = BeautifulSoup(response.text, 'html.parser')
-        links = []
-        for link in soup.find_all('a', href=True):
-            href = link['href']
-            if href.startswith('https://click.linksynergy.com/'): #udemy link
-                links.append(href)
-    return links
 
 def main():
     pass
